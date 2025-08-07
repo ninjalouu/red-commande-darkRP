75988f7bf05539b64c28863bdc9769083abf1bff
ninjalouu <redcommandrp@gmail.com>
2025-08-07 10:58:13 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 
-// ⚠️ Remplace par les infos de ton projet Firebase
+// Configuration Firebase pour RED COMMAND RP
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "XXXXXX"
+  apiKey: "AIzaSyBX8O16331ikac8PQ_biEu8c1KTGoA6Fhk",
+  authDomain: "red-command-darkrp-889fd.firebaseapp.com",
+  projectId: "red-command-darkrp-889fd",
+  storageBucket: "red-command-darkrp-889fd.firebasestorage.app",
+  messagingSenderId: "186231864744",
+  appId: "1:186231864744:web:98aa32b8eea2f18b67f19d"
 };
 
+// Initialisation Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
