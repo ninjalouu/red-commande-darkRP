cdd1fc9d2d353f4c271fb9d24ef5a369eccb294f
ninjalouu <redcommandrp@gmail.com>
2025-08-07 11:56:07 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX8O16331ikac8PQ_biEu8c1KTGoA6Fhk",
+  authDomain: "red-command-darkrp-889fd.firebaseapp.com",
+  projectId: "red-command-darkrp-889fd",
+  storageBucket: "red-command-darkrp-889fd.firebasestorage.app",
+  messagingSenderId: "186231864744",
+  appId: "1:186231864744:web:98aa32b8eea2f18b67f19d"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
